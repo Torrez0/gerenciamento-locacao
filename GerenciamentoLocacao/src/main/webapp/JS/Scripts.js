f9b74b1d16cb561f10a9d8f252dd0d1ee7897877
TarsPer <rafaelsilva106@outlook.com>
2023-10-09 17:43:06 -0300

    Dialog em JS e HTML criado para Botao LOGIN

---
+++ GerenciamentoLocacao/src/main/webapp/JS/Scripts.js
@@ -0,0 +1,18 @@
+//Funcão para abrir a tela de login
+
+function abrirLoginDialog(){
+
+    var loginDialog = document.getElementById('loginDialog');
+   
+        loginDialog.showModal();
+    
+}
+
+//Função para fechar tela login
+function fecharLoginDialog(){
+
+    var loginDialog = document.getElementById('loginDialog');
+        
+        loginDialog.close();
+    
+}
