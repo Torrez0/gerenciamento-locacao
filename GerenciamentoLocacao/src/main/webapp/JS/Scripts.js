cd316b7faf6763855d4ea87b736aab018f80d79e
brian.bzculque <brianzav23????@>
2023-10-09 19:00:56 -0300

    Arrumando branch

--- GerenciamentoLocacao/src/main/webapp/JS/Scripts.js
+++ GerenciamentoLocacao/src/main/webapp/JS/Scripts.js
@@ -3,16 +3,33 @@
 function abrirLoginDialog(){
 
     var loginDialog = document.getElementById('loginDialog');
-   
+
         loginDialog.showModal();
-    
+
 }
 
 //Função para fechar tela login
 function fecharLoginDialog(){
 
     var loginDialog = document.getElementById('loginDialog');
-        
+
         loginDialog.close();
-    
+
+}
+
+function abrirCadastroDialog(){
+
+    var CadastroDialog = document.getElementById('CadastroDialog');
+
+        CadastroDialog.showModal();
+
+}
+
+//Função para fechar tela login
+function fecharCadastroDialog(){
+
+    var CadastroDialog = document.getElementById('CadastroDialog');
+
+        CadastroDialog.close();
+
 }
